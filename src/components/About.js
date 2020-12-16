0e66b594bfe581b7d6e448df0289b06b4d83e975
baromeg <baromeg@gmail.com>
2020-12-16 22:47:08 +0000

    Hello World

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const About = () => {
+
+
+  return <section className="hero is-fullheight-with-navbar is-info">
+    <div className="hero-body">
+      <div className="container">
+        <div className="section title">
+          Hello About
+        </div>
+      </div>
+
+
+    </div>
+
+
+
+  </section>
+
+
+}
+
+export default About
